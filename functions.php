771122330140b2202256aa2caf0b4d9a86fa555b
minamikawa <minami_pue@yahoo.co.jp>
2017-03-09 14:35:06 +0900

    ウィジェット、メニュー、ぱんくず、など。

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/** ダイナミックサイドバーを定義するコード（CHAPTER 11）*/
+register_sidebar( array(
+	'name'			=> 'サイドバーウィジット-1',
+	'id'			=> 'sidebar-1',
+	'description'	=> 'サイドバーのウィジットエリアです。デフォルトのサイドバーと丸ごと入れ替えたいときに使ってください。',
+    'before_widget'	=> '<div id="%1$s" class="widget %2$s">',
+    'after_widget'	=> '</div>',
+) );
+
+/** カスタムメニューの「場所」を設定するコード */
+register_nav_menu( 'header-navi', 'ヘッダーのナビゲーション' );
+
+?>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Bootstrap Sample</title>
+<title><?php bloginfo( 'name' ); ?></title>
 
 <!-- BootstrapのCSS読み込み -->
 <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" rel="stylesheet">
@@ -29,6 +29,23 @@
 
 <h1><?php bloginfo( 'name' ); ?></h1>
 
+<div>
+<?php wp_nav_menu( array( 'theme_location = header-navi' ) ); ?>
+</div>
+
+<div>
+<?php if( !is_front_page() ){ ?>
+<ul class="breadcrumb">
+<li>
+<a href="<?php echo esc_url( home_url() ); ?>">
+HOME
+</a>
+</li>
+<li class="active"><?php the_title(); ?></li>
+</ul>
+<?php } ?>
+</div>
+
 <div class="row">
 <div class="col-xs-12 col-sm-6">
 
@@ -51,14 +68,20 @@ else: /** ここから記事が見つからなかった場合の処理 */ ?>
 </div>
 <div class="col-xs-12 col-sm-6">
 <div class="visible-xs" style="height:20px;"></div>
-<h3 style="margin-top:0">XXX</h3>
-<p>YYY</p>
+<div>
+<?php dynamic_sidebar('sidebar-1'); ?>
 </div>
+</div>
+</div>
+
+<div id="footer">
+&copy; <?php bloginfo( 'name' ); ?> All Rights Reserved.
 </div>
 
 </div><!-- 全体を囲むコンテナ -->
 
 
 
+<?php wp_footer(); ?>
 </body>
 </html>
