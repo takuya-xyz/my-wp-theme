42651f4346c6bd1fdc2c4ac5988e59c2f675de83
minamikawa <minami_pue@yahoo.co.jp>
2017-03-23 23:39:28 +0900

    Bootstrapのスライダーのトップ画像を設置

--- front-page.php
+++ front-page.php
@@ -74,6 +74,51 @@ WEBサイト制作<br />
 
 <div id="front_center_box">
 
+
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+ 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/top_image/top_image_1.jpg" alt="写真1">
+      <div class="carousel-caption">
+        <h1></h1>
+      </div>
+    </div>
+    <div class="item">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/top_image/top_image_2.jpg" alt="写真2">
+      <div class="carousel-caption">
+        <h1></h1>
+      </div>
+    </div>
+    <div class="item">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/top_image/top_image_3.jpg" alt="写真3">
+      <div class="carousel-caption">
+        <h1></h1>
+      </div>
+    </div>
+  </div>
+ 
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div style="height:20px;background:#fff;"></div>
+
 <h2>WEBサイト制作に関することならなんでもお気軽にご相談ください</h2>
 
 <div id="self-introduction">
@@ -142,7 +187,6 @@ echo nl2br($html);
 
 <div class="hidden-xs">
 <div id="side_fix_test_3">
-後半は変動。
 </div>
 </div>
 
@@ -240,7 +284,7 @@ WEBサイト制作<br />
 <div class="hidden-xs">
 
 <ul>
-<li style="font-size:10px;color:#fff;">
+<li style="font-size:10px;color:#fff;line-height:120%;padding-top:8px;">
 WEBサイト制作<br />
 アウトドアチャイルド
 </li>
